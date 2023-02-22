d8fb4249e025fdbe01887bda2dbeece0dcf9efc5
Ty Stanford <tystan@gmail.com>
2023-02-22 19:42:17 +1030

    adu analysis added to supp file

--- r/2_compile_results.R
+++ r/2_compile_results.R
@@ -28,12 +28,20 @@ fig_y_scale <- function(y1, y2, by_val = 3) {
   scale_y_continuous(limits = c(y1, y2), breaks = seq(y1, y2, by = by_val))
 }
 
-fig_bmi_y_scale <- function() {
+fig_chi_bmi_y_scale <- function() {
   fig_y_scale(14, 28)
 }
 
-fig_ovo_y_scale <- function() {
+fig_adu_bmi_y_scale <- function() {
+  fig_y_scale(15, 40)
+}
+
+fig_chi_ovo_y_scale <- function() {
   fig_y_scale(0, 0.75, 0.15)
+}
+
+fig_adu_ovo_y_scale <- function() {
+  fig_y_scale(0, 1, 0.2)
 }
 
 fig_sei_scale <- function() {
@@ -128,7 +136,7 @@ prs_sei_plot <-
     alpha = 0.9
   ) +
   fig_sei_scale() +
-  fig_bmi_y_scale() +
+  fig_chi_bmi_y_scale() +
   fig_theme() +
   labs(
     col = "SEIFA\nquintile",
@@ -217,7 +225,7 @@ prs_sep_plot <-
     alpha = 0.9
   ) +
   fig_sep_scale() +
-  fig_bmi_y_scale() +
+  fig_chi_bmi_y_scale() +
   fig_theme() +
   labs(
     col = "SEP\nquintile",
@@ -297,7 +305,7 @@ sei_plot <-
     alpha = 0.9
   ) +
   fig_sei_scale() +
-  fig_bmi_y_scale() +
+  fig_chi_bmi_y_scale() +
   fig_theme() +
   labs(
     col = "SEIFA\nquintile",
@@ -364,7 +372,7 @@ sep_plot <-
     alpha = 0.9
   ) +
   fig_sep_scale() +
-  fig_bmi_y_scale() +
+  fig_chi_bmi_y_scale() +
   fig_theme() +
   labs(
     col = "SEP\nquintile",
@@ -379,7 +387,7 @@ ggarrange(sei_plot, sep_plot, nrow = 1, labels = c("(A)", "(B)"))
 
 
 ggsave(
-  filename = "fig/supp_fig_3.png",
+  filename = "fig/supp_fig_chi_bmi.png",
   width = 12,
   height = 7,
   dpi = 300
@@ -472,7 +480,7 @@ prs_sei_plot <-
     alpha = 0.9
   ) +
   fig_sei_scale() +
-  fig_ovo_y_scale() +
+  fig_chi_ovo_y_scale() +
   fig_theme() +
   labs(
     col = "SEIFA\nquintile",
@@ -562,7 +570,7 @@ prs_sep_plot <-
     alpha = 0.9
   ) +
   fig_sep_scale() +
-  fig_ovo_y_scale() +
+  fig_chi_ovo_y_scale() +
   fig_theme() +
   labs(
     col = "SEP\nquintile",
@@ -641,7 +649,7 @@ sei_plot <-
     alpha = 0.9
   ) +
   fig_sei_scale() +
-  fig_ovo_y_scale() +
+  fig_chi_ovo_y_scale() +
   fig_theme() +
   labs(
     col = "SEIFA\nquintile",
@@ -708,7 +716,7 @@ sep_plot <-
     alpha = 0.9
   ) +
   fig_sep_scale() +
-  fig_ovo_y_scale() +
+  fig_chi_ovo_y_scale() +
   fig_theme() +
   labs(
     col = "SEP\nquintile",
@@ -724,7 +732,7 @@ ggarrange(sei_plot, sep_plot, nrow = 1, labels = c("(A)", "(B)"))
 
 
 ggsave(
-  filename = "fig/supp_fig_4.png",
+  filename = "fig/supp_fig_chi_ovo.png",
   width = 12,
   height = 7,
   dpi = 300
@@ -745,6 +753,676 @@ ggsave(
 
 
 
+
+# ---- adu_bmi_prs_sei_tab ----
+
+# sei plot
+
+df_preds <- arrow::read_parquet("res/mod_adu_bmi_sei_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds <-
+  df_preds %>%
+  group_by(sei, prs, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sei, prs, age_cat)
+
+
+boot_df_preds <- arrow::read_parquet("res/mod_adu_bmi_sei_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci <-
+  boot_df_preds %>%
+  group_by(sei, prs, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sei, prs, age_cat)
+
+adu_bmi_sei_tab_dat <-
+  full_join(
+    df_preds,
+    boot_df_ci,
+    c("sei", "prs", "age_cat")
+  )
+
+adu_bmi_sei_tab_dat %>%
+  mutate(est_ci = sprintf("%2.1f (%2.1f, %2.1f)", mean_p, lo_p, up_p)) %>%
+  select(sei, prs, age_cat, est_ci) %>%
+  pivot_wider(., names_from = age_cat, values_from = est_ci) %>%
+  kable(.)
+
+
+
+
+# ---- adu_bmi_prs_sei_fig ----
+
+prs_sei_plot <-
+  adu_bmi_sei_tab_dat %>%
+  rename(`PRS quintile` = prs) %>%
+  ggplot(., aes(x = age_cat, group = sei, fill = sei)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  facet_wrap(`PRS quintile` ~ ., labeller = "label_both", ncol = 5) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sei, col = sei),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sei),
+    alpha = 0.9
+  ) +
+  fig_sei_scale() +
+  fig_adu_bmi_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEIFA\nquintile",
+    fill = "SEIFA\nquintile",
+    x = "Age category",
+    y = "BMI"
+  ) 
+
+ggsave(
+  plot = prs_sei_plot,
+  filename = "fig/supp_fig_adu_bmi_prs_sei.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
+
+
+print(prs_sei_plot)
+
+
+# ---- adu_bmi_prs_sep_tab ----
+
+
+
+# sep plot
+
+df_preds <- arrow::read_parquet("res/mod_adu_bmi_sep_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds <-
+  df_preds %>%
+  group_by(sep, prs, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sep, prs, age_cat)
+
+
+boot_df_preds <- arrow::read_parquet("res/mod_adu_bmi_sep_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci <-
+  boot_df_preds %>%
+  group_by(sep, prs, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2, na.rm = TRUE), 
+    up_p = quantile(pred_p, 1 - alpha / 2, na.rm = TRUE), 
+    .groups = "drop"
+  ) %>%
+  arrange(sep, prs, age_cat)
+
+adu_bmi_sep_tab_dat <-
+  full_join(
+    df_preds,
+    boot_df_ci,
+    c("sep", "prs", "age_cat")
+  )
+
+adu_bmi_sep_tab_dat %>%
+  mutate(est_ci = sprintf("%2.1f (%2.1f, %2.1f)", mean_p, lo_p, up_p)) %>%
+  select(sep, prs, age_cat, est_ci) %>%
+  pivot_wider(., names_from = age_cat, values_from = est_ci) %>%
+  kable(.) 
+  # print(., n = nrow(.))
+
+
+
+# ---- adu_bmi_prs_sep_fig ----
+
+prs_sep_plot <-
+  adu_bmi_sep_tab_dat %>%
+  rename(`PRS quintile` = prs) %>%
+  ggplot(., aes(x = age_cat, group = sep, fill = sep)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  facet_wrap(`PRS quintile` ~ ., labeller = "label_both", ncol = 5) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sep, col = sep),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sep),
+    alpha = 0.9
+  ) +
+  fig_sep_scale() +
+  fig_adu_bmi_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEP\nquintile",
+    fill = "SEP\nquintile",
+    x = "Age category",
+    y = "BMI"
+  ) 
+
+ggsave(
+  plot = prs_sep_plot,
+  filename = "fig/supp_fig_adu_bmi_prs_sep.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
+
+print(prs_sep_plot)
+
+
+
+
+# ---- adu_bmi_plot ----
+
+
+
+# sei plot
+
+df_preds_sei <- arrow::read_parquet("res/mod_adu_bmi_sei_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds_sei <-
+  df_preds_sei %>%
+  group_by(sei, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sei, age_cat)
+
+
+boot_df_preds_sei <- arrow::read_parquet("res/mod_adu_bmi_sei_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci_sei <-
+  boot_df_preds_sei %>%
+  group_by(sei, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sei, age_cat)
+
+plot_dat_sei <-
+  full_join(
+    df_preds_sei,
+    boot_df_ci_sei,
+    c("sei", "age_cat")
+  )
+
+
+
+sei_plot <-
+  plot_dat_sei %>%
+  ggplot(., aes(x = age_cat, group = sei, fill = sei)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sei, col = sei),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sei),
+    alpha = 0.9
+  ) +
+  fig_sei_scale() +
+  fig_adu_bmi_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEIFA\nquintile",
+    fill = "SEIFA\nquintile",
+    x = "Age category",
+    y = "BMI"
+  ) 
+
+
+
+
+# sep plot
+
+df_preds_sep <- arrow::read_parquet("res/mod_adu_bmi_sep_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds_sep <-
+  df_preds_sep %>%
+  group_by(sep, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sep, age_cat)
+
+
+
+boot_df_preds_sep <- arrow::read_parquet("res/mod_adu_bmi_sep_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci_sep <-
+  boot_df_preds_sep %>%
+  group_by(sep, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sep, age_cat)
+
+plot_dat_sep <-
+  full_join(
+    df_preds_sep,
+    boot_df_ci_sep,
+    c("sep", "age_cat")
+  )
+
+
+
+sep_plot <-
+  plot_dat_sep %>%
+  ggplot(., aes(x = age_cat, group = sep, fill = sep)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sep, col = sep),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sep),
+    alpha = 0.9
+  ) +
+  fig_sep_scale() +
+  fig_adu_bmi_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEP\nquintile",
+    fill = "SEP\nquintile",
+    x = "Age category",
+    y = "BMI"
+  ) 
+
+
+
+ggarrange(sei_plot, sep_plot, nrow = 1, labels = c("(A)", "(B)")) 
+
+
+ggsave(
+  filename = "fig/supp_fig_adu_bmi.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---- adu_ovo_prs_sei_tab ----
+
+# sei plot
+
+df_preds <- arrow::read_parquet("res/mod_adu_ovo_sei_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds <-
+  df_preds %>%
+  group_by(sei, prs, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sei, prs, age_cat)
+
+
+boot_df_preds <- arrow::read_parquet("res/mod_adu_ovo_sei_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci <-
+  boot_df_preds %>%
+  group_by(sei, prs, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sei, prs, age_cat)
+
+adu_ovo_sei_tab_dat <-
+  full_join(
+    df_preds,
+    boot_df_ci,
+    c("sei", "prs", "age_cat")
+  )
+
+adu_ovo_sei_tab_dat %>%
+  mutate(est_ci = sprintf("%0.2f (%0.2f, %0.2f)", mean_p, lo_p, up_p)) %>%
+  select(sei, prs, age_cat, est_ci) %>%
+  pivot_wider(., names_from = age_cat, values_from = est_ci) %>%
+  kable(.)
+
+
+
+
+# ---- adu_ovo_prs_sei_fig ----
+
+
+prs_sei_plot <-
+  adu_ovo_sei_tab_dat %>%
+  rename(`PRS quintile` = prs) %>%
+  ggplot(., aes(x = age_cat, group = sei, fill = sei)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  facet_wrap(`PRS quintile` ~ ., labeller = "label_both", ncol = 5) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sei, col = sei),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sei),
+    alpha = 0.9
+  ) +
+  fig_sei_scale() +
+  fig_adu_ovo_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEIFA\nquintile",
+    fill = "SEIFA\nquintile",
+    x = "Age category",
+    y = "Probability of overweight/obese"
+  ) 
+
+ggsave(
+  plot = prs_sei_plot,
+  filename = "fig/supp_fig_adu_ovo_prs_sei.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
+
+
+print(prs_sei_plot)
+
+
+# ---- adu_ovo_prs_sep_tab ----
+
+
+
+# sep plot
+
+df_preds <- arrow::read_parquet("res/mod_adu_ovo_sep_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds <-
+  df_preds %>%
+  group_by(sep, prs, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sep, prs, age_cat)
+
+
+boot_df_preds <- arrow::read_parquet("res/mod_adu_ovo_sep_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci <-
+  boot_df_preds %>%
+  group_by(sep, prs, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sep, prs, age_cat)
+
+adu_ovo_sep_tab_dat <-
+  full_join(
+    df_preds,
+    boot_df_ci,
+    c("sep", "prs", "age_cat")
+  )
+
+adu_ovo_sep_tab_dat %>%
+  mutate(est_ci = sprintf("%0.2f (%0.2f, %0.2f)", mean_p, lo_p, up_p)) %>%
+  select(sep, prs, age_cat, est_ci) %>%
+  pivot_wider(., names_from = age_cat, values_from = est_ci) %>%
+  kable(.)
+
+
+# ---- adu_ovo_prs_sep_fig ----
+
+
+
+prs_sep_plot <-
+  adu_ovo_sep_tab_dat %>%
+  rename(`PRS quintile` = prs) %>%
+  ggplot(., aes(x = age_cat, group = sep, fill = sep)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  facet_wrap(`PRS quintile` ~ ., labeller = "label_both", ncol = 5) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sep, col = sep),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sep),
+    alpha = 0.9
+  ) +
+  fig_sep_scale() +
+  fig_adu_ovo_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEP\nquintile",
+    fill = "SEP\nquintile",
+    x = "Age category",
+    y = "Probability of overweight/obese"
+  ) 
+
+ggsave(
+  plot = prs_sep_plot,
+  filename = "fig/supp_fig_adu_ovo_prs_sep.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
+
+
+print(prs_sep_plot)
+
+
+# ---- adu_ovo_plot ----
+
+
+
+# sei plot
+
+df_preds_sei <- arrow::read_parquet("res/mod_adu_ovo_sei_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds_sei <-
+  df_preds_sei %>%
+  group_by(sei, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sei, age_cat)
+
+
+boot_df_preds_sei <- arrow::read_parquet("res/mod_adu_ovo_sei_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci_sei <-
+  boot_df_preds_sei %>%
+  group_by(sei, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sei, age_cat)
+
+plot_dat_sei <-
+  full_join(
+    df_preds_sei,
+    boot_df_ci_sei,
+    c("sei", "age_cat")
+  )
+
+
+
+sei_plot <-
+  plot_dat_sei %>%
+  ggplot(., aes(x = age_cat, group = sei, fill = sei)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  geom_line(
+    aes(x = age_cat, y = mean_p, group = sei, col = sei),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = mean_p, col = sei),
+    alpha = 0.9
+  ) +
+  fig_sei_scale() +
+  fig_adu_ovo_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEIFA\nquintile",
+    fill = "SEIFA\nquintile",
+    x = "Age category",
+    y = "Probability of overweight/obese"
+  )
+
+
+
+
+# sep plot
+
+df_preds_sep <- arrow::read_parquet("res/mod_adu_ovo_sep_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+df_preds_sep <-
+  df_preds_sep %>%
+  group_by(sep, age_cat) %>%
+  summarise(
+    mean_p = mean(pred_p),
+    .groups = "drop"
+  ) %>%
+  arrange(sep, age_cat)
+
+
+
+boot_df_preds_sep <- arrow::read_parquet("res/mod_adu_ovo_sep_boot_preds.parquet")
+
+# assuming equal gender and prs quintile weightings, 
+# plain average is what we are after
+boot_df_ci_sep <-
+  boot_df_preds_sep %>%
+  group_by(sep, age_cat) %>%
+  summarise(
+    n_boot = n(), 
+    med_p = median(pred_p), 
+    lo_p = quantile(pred_p, alpha / 2), 
+    up_p = quantile(pred_p, 1 - alpha / 2), 
+    .groups = "drop"
+  ) %>%
+  arrange(sep, age_cat)
+
+plot_dat_sep <-
+  full_join(
+    df_preds_sep,
+    boot_df_ci_sep,
+    c("sep", "age_cat")
+  )
+
+
+
+sep_plot <-
+  plot_dat_sep %>%
+  ggplot(., aes(x = age_cat, group = sep, fill = sep)) +
+  geom_ribbon(aes(ymin = lo_p, ymax = up_p), alpha = 0.1) +
+  geom_line(
+    aes(x = age_cat, y = med_p, group = sep, col = sep),
+    alpha = 0.6
+  ) +
+  geom_point(
+    aes(x = age_cat, y = med_p, col = sep),
+    alpha = 0.9
+  ) +
+  fig_sep_scale() +
+  fig_adu_ovo_y_scale() +
+  fig_theme() +
+  labs(
+    col = "SEP\nquintile",
+    fill = "SEP\nquintile",
+    x = "Age category",
+    y = "Probability of overweight/obese"
+  ) 
+
+
+
+
+ggarrange(sei_plot, sep_plot, nrow = 1, labels = c("(A)", "(B)")) 
+
+
+ggsave(
+  filename = "fig/supp_fig_adu_ovo.png",
+  width = 12,
+  height = 7,
+  dpi = 300
+)
 
 
 
